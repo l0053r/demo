0472f0f66fad29159f0313049aa456071da9dfc1
l0053r <karan70053r@protonmail.com>
2025-12-31 16:39:22 +0530

    message modified

--- src/main/java/com/example/app/controller/TestController.java
+++ src/main/java/com/example/app/controller/TestController.java
@@ -1,12 +1,13 @@
 package com.example.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     @GetMapping("/hello")
-    public String getGreeting(){
-        return "hello world";
+    public String getGreeting(@PathVariable String name){
+        return "hello "+name+" happy new year 2026";
     }
 }
