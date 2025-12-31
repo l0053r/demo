45dad4270980717d9aaf13391c4c52c4772a22df
l0053r <karan70053r@protonmail.com>
2025-12-31 12:09:14 +0530

    first commit

---
+++ src/main/java/com/example/app/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String getGreeting(){
+        System.out.println("hello karan");
+        return "hello karan";
+    }
+}
